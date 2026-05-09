1553f072c185e7b02acf7aeffcb243f33970f5d8
Ngô Lê Thanh Toàn <thanhtoan22082005@gmail.com>
2026-05-09 15:58:07 +0700

    Add Student project files and dependencies

---
+++ Student/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student.Models;
+namespace Student.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ShowKQ(StudentModel sv)
+        {
+            // Giả lập số lượng sinh viên cùng ngành
+            ViewBag.SoLuong = new Random().Next(1, 100);
+            return View(sv);
+        }
+    }
+}
---
+++ Student/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Student.Models
+{
+    public class StudentModel
+    {
+        public string MSSV { get; set; }
+        public string HoTen { get; set; }
+        public double DiemTB { get; set; }
+        public string ChuyenNganh { get; set; }
+    }
+}
